3dabc03c4586d86befdfb59a025fc198691427b0
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-04-04 15:13:10 -0300

    Organizando a Solucao

---
+++ RafaelStore/RafaelStore.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RafaelStore.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
