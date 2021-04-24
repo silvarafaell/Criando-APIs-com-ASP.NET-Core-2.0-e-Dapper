5685a2bf682e35f100227bdc2c5d6b3f30245247
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-04-23 23:43:20 -0300

    Modelando o pedido

--- RafaelStore/RafaelStore.Domain/StoreContext/Entities/Delivery.cs
+++ RafaelStore/RafaelStore.Domain/StoreContext/Entities/Delivery.cs
@@ -14,5 +14,17 @@ namespace RafaelStore.Domain.StoreContext.Entities
         public DateTime CreateDate { get; private set; }
         public DateTime EstimatedDeliveryDate { get; private set; }
         public EDeliveryStatus Status { get; private set; }
+
+        public void Ship()
+        {
+            //Se a Data estimada de entrega for no passado, não entregar
+            Status = EDeliveryStatus.Shipped;
+        }
+
+        public void Cancel()
+        {
+            //Se o status ja estiver entregue, não pode cancelar
+            Status = EDeliveryStatus.Canceled;
+        }
     }
 }
--- RafaelStore/RafaelStore.Tests/UnitTest1.cs
+++ RafaelStore/RafaelStore.Tests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace RafaelStore.Tests
         public void TestMethod1()
         {
             //Instancia do Objeto
-            var c = new Customer("Francisco", "Rafael", "12345678", "francisco_rafael@hotmail.com.br", "8596029268", "Rua dois n 101");
+            var c = new Customer();
 
             var order = new Order(c);
         }
