a728873c5d3255c0859816c134ae1a59f3d184b3
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-04-25 10:20:14 -0300

    Simulando o pedido

--- RafaelStore/RafaelStore.Tests/UnitTest1.cs
+++ RafaelStore/RafaelStore.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RafaelStore.Domain.StoreContext.Entities;
+using RafaelStore.Domain.StoreContext.ValueObjects;
 
 namespace RafaelStore.Tests
 {
@@ -9,10 +10,33 @@ namespace RafaelStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var name = new Name("Francisco", "Rafael");
+            var document = new Document("12345678911");
+            var email = new Email("francisco_rafael@hotmail.com.br");
             //Instancia do Objeto
-            var c = new Customer();
+            var c = new Customer(name, document, email, "85996029268");
+            var mouse = new Product("Mouse", "Rato", "Image.png", 59.90M, 10);
+            var teclado = new Product("Teclado", "Teclado", "Image.png", 159.90M, 10);
+            var impressora = new Product("Impressora", "Impressora", "Image.png", 359.90M, 10);
+            var cadeira = new Product("Cadeira", "Cadeira", "Image.png", 559.90M, 10);
 
             var order = new Order(c);
+            order.AddItem(new OrderItem(mouse, 5));
+            order.AddItem(new OrderItem(teclado, 5));
+            order.AddItem(new OrderItem(cadeira, 5));
+            order.AddItem(new OrderItem(impressora, 5));
+
+            //Realizei o pedido
+            order.Place();
+
+            //Simular o pagamento
+            order.Pay();
+
+            //Simular o envio
+            order.Ship();
+
+            //Simular o cancelamento
+            order.cancel();
         }
     }
 }
